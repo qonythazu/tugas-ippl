b7a953f381b0b4020b6924e182a2ab8fedaa1ca7
qonythazu <qonitaarif5@gmail.com>
2022-10-01 14:50:42 +0800

    Screen detail notes dah jadi anjay

--- lib/detailpesanan.dart
+++ lib/detailpesanan.dart
@@ -28,10 +28,123 @@ class _DetailPesananState extends State<DetailPesanan> {
       ),
       body: Container(
         child: Center(
-          child: Text(
-            widget.idPesanan.toString()
-          )
-        ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Kemeja",
+                  style: TextStyle(
+                    color: Color(0xffD9534F),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xffD9534F)),
+                  borderRadius: BorderRadius.circular(8)
+                ),
+                margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Bahu", style: TextStyle(color: Color(0xffFFAD60)),),
+                    Spacer(),
+                    Text("20", style: TextStyle(color: Color(0xffD9534F)),)
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xffD9534F)),
+                  borderRadius: BorderRadius.circular(8)
+                ),
+                margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Panjang Lengan", style: TextStyle(color: Color(0xffFFAD60)),),
+                    Spacer(),
+                    Text("57", style: TextStyle(color: Color(0xffD9534F)),)
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xffD9534F)),
+                  borderRadius: BorderRadius.circular(8)
+                ),
+                margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Lingkar Dada", style: TextStyle(color: Color(0xffFFAD60)),),
+                    Spacer(),
+                    Text("90", style: TextStyle(color: Color(0xffD9534F)),)
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xffD9534F)),
+                  borderRadius: BorderRadius.circular(8)
+                ),
+                margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Panjang baju", style: TextStyle(color: Color(0xffFFAD60)),),
+                    Spacer(),
+                    Text("64", style: TextStyle(color: Color(0xffD9534F)),)
+                  ],
+                ),
+              ),
+              Spacer(),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        elevation: 0
+                      ),
+                      onPressed: () {
+
+                      }, 
+                      icon: const Icon(Icons.delete, color: Color(0xffD9534F),), 
+                      label: const Text("DELETE", style: TextStyle(color: Color(0xffD9534F)),)
+                    ),
+                     ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        elevation: 0
+                      ),
+                      onPressed: () {
+
+                      }, 
+                      icon: const Icon(Icons.edit, color: Color(0xffFFAD60),), 
+                      label: const Text("EDIT", style: TextStyle(color: Color(0xffFFAD60)),)
+                    )
+                  ],
+                ),
+              )
+
+            ],
+          ),
+        )
       ),
     );
   }
